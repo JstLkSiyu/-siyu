0a9d500065a7501bc643c89f78bc3c840a37e985
林泽鹏 <linzepeng.siyu@bytedance.com>
2021-10-20 20:53:34 +0800

    chore: update initStore demo3

--- src/demo/initStore.demo3.tsx
+++ src/demo/initStore.demo3.tsx
@@ -10,14 +10,17 @@ const store = {
     name: 'kirito',
     age: 17
   },
-  arr: [1,2,3]
+  arr: [1,2,3],
+  obj: {
+    hello: 'world'
+  }
 };
 
 const useStore = initStore(store);
 
 const Demo: FC<any> = props => {
   const { store } = useStore();
-  const { arr, obj1, obj2 } = store;
+  const { arr, obj1, obj2, obj } = store;
   return (
     <div>
       <div>
@@ -28,10 +31,21 @@ const Demo: FC<any> = props => {
         <h3>{obj2.name}</h3>
         <b>{obj2.age} years</b>
       </div>
+      <h3>{arr.join(',')}</h3>
+      <h3>{Object.values(obj).join(',')}</h3>
       <div>
         <button onClick={() => {
           [store.obj2, store.obj1] = [store.obj1, store.obj2];
         }}>exchange obj1 and obj2</button>
+        <button onClick={() => {
+          store.arr = arr;
+        }}>set arr to itself</button>
+        <button onClick={() => {
+          store.arr.push(4);
+        }}>extend arr</button>
+        <button onClick={() => {
+          store.obj = Object.assign(obj, { [Math.random()]: 'world' });
+        }}>extend obj</button>
       </div>
     </div>
   )
